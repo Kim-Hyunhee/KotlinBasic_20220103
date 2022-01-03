5aea8abd911ad20cd28a9f9f6a2fca7ee0dd66b0
김현희 <tta_bi@naver.com>
2022-01-03 16:38:23 +0900

    텍스트뷰의 UI를 코틀린에서 변경해보기 -text 송성에 들어갈 값 새로 대입

--- app/src/main/java/com/nepplus/kotlinbasic_20220103/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20220103/MainActivity.kt
@@ -11,30 +11,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-// log button play
+// 로그 버튼 띄우기
         btnLog.setOnClickListener {
 
 //
-            Log.d("main","log")
-            Log.e("main","log e")
+            Log.d("main","로그")
+            Log.e("main","로그 e")
 
         }
-    // toast button
+    // 토스트 버튼 누르기
         btnToast.setOnClickListener {
-// "hello" sentence
-            Toast.makeText(this, "hello~", Toast.LENGTH_SHORT).show()
+// 토스트로 "안녕하세요." 문장 띄우기
+            Toast.makeText(this, "안녕하세요.", Toast.LENGTH_SHORT).show()
 
         }
         
         btnMessageToast.setOnClickListener {
 
-            // 1. edtMessage sentence => variety save
-            val inputMessage = edtMessage.text.toString()   // input sentence =>string =>variety save
+            // 1. edtMessage에 입력된 문구를 받아서 => 변수에 저장해두자
+            val inputMessage = edtMessage.text.toString()   // 입력된 문구(text)를 변수에 저장하자
 
-            // 2. inputMessage => String interleave
-            // Python : f'str
+            // 2. inputMessage변수에 담긴 값을 String에 끼워넣기
+            // Python : f'str 사용방법
 
-            Toast.makeText(this, "input ${inputMessage}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "${inputMessage} 입력됨", Toast.LENGTH_SHORT).show()
+        }
+
+        btnMessageTextView.setOnClickListener {
+
+            // 1. 입력된 문구를 받아서 저장 Python : input()
+            val inputMessage = edtMessage.text.toString()
+
+            // 2. txtMessage의 문구로 inputMessage 반영
+            txtMessage.text = "${inputMessage} 입력 되었다."
         }
 
     }
